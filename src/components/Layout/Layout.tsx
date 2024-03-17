e1888520933a005b2ce1416b6bd19c161ed8ec54
Nikolay Smekalin <nikolay.smekalin@gmail.com>
2024-03-17 14:44:09 +0300

    feat: add Loyout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { memo } from "react";
+
+import { Outlet } from "react-router-dom";
+
+import styles from './Layout.module.scss';
+
+function Layout() {
+  return (
+    <div className={styles.layout}>
+      <Outlet />
+    </div>
+  );
+}
+
+export default memo(Layout);
