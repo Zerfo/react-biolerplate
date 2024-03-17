59245414a042f741ac87983c46bbbf9f25326573
Nikolay Smekalin <nikolay.smekalin@gmail.com>
2024-03-17 14:51:42 +0300

    feat: add App component

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,31 @@
+import { memo, Suspense, useMemo } from 'react';
+
+import RouterDesktop from 'containers/Router/ReouterDesktop';
+import RouterMobile from 'containers/Router/RouterMobile';
+import RouterTablet from 'containers/Router/RouterTablet';
+
+import useBreakpoints, { BreakPoints } from 'hooks/useBreakpoint';
+
+function App() {
+  const breakpoints = useBreakpoints();
+
+  const loyoutSwitch = useMemo(() => {
+    switch (breakpoints) {
+      case BreakPoints.MOBILE:
+        return <RouterMobile />;
+      case BreakPoints.TABLET:
+        return <RouterTablet />;
+      case BreakPoints.DESKTOP:
+      default:
+        return <RouterDesktop />;
+    }
+  }, [breakpoints]);
+
+  return (
+    <Suspense fallback={<div>....Loading</div>}>
+      {loyoutSwitch}
+    </Suspense>
+  );
+}
+
+export default memo(App);
