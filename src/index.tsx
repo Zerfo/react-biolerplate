86c37299ef4f58b983f84931e52b6135b441f75e
Nikolay Smekalin <nikolay.smekalin@gmail.com>
2024-03-17 15:14:58 +0300

    feat: add base scss files

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client';
 
 import Root from 'containers/Root';
 
+import 'styles/index.scss';
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(<Root />);
